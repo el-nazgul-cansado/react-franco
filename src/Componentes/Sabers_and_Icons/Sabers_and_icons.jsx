ab6f9b1f03768a9fdde6210e1cdfedb23b7c527d
Franco J Beccari <francojoaquinbeccari@gmail.com>
2024-03-22 10:43:45 -0300

    Corregido bug donde no iniciaba la página por error en la lectura de los iconos

--- src/Componentes/Sabers_and_Icons/Sabers_and_icons.jsx
+++ src/Componentes/Sabers_and_Icons/Sabers_and_icons.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { sabers_and_icons_data } from '../../helpers/pedirDatos';
-import './Sabers_and_icons.css'; // Asegúrate de tener este archivo CSS
+import './Sabers_and_icons.css';
 
 export const Sabers_and_icons = () => {
     const [sabersAndIcons, setSabersAndIcons] = useState([]);
@@ -10,8 +10,10 @@ export const Sabers_and_icons = () => {
       // Llamada a la función que retorna la promesa con los datos
       sabers_and_icons_data()
         .then((res) => {
-          setSabersAndIcons(res); // Actualizar el estado con la respuesta
-          setSelectedIcon(res[0]); // Establecer el ícono seleccionado inicialmente al primer elemento
+          setSabersAndIcons(res);
+          if (res.length > 0) {
+            setSelectedIcon(res[0]); // Asegúrate de que 'res' no está vacío
+          }
         })
         .catch((error) => {
           console.error("Error cargando los datos de los íconos:", error);
@@ -21,18 +23,21 @@ export const Sabers_and_icons = () => {
     const handleIconClick = (icon) => {
       setSelectedIcon(icon);
     };
-  
+
+    // Verifica que selectedIcon no sea null antes de intentar acceder a sus propiedades
     return (
         <div className="dropdown">
-            <img src={selectedIcon.icon} alt={selectedIcon.name} className="selected-icon" />
-                <div className="dropdown-content">
-                    {sabersAndIcons.map((e, index) => (
-                        <div key={index} className="icon-saber-pair" onClick={() => handleIconClick(e)}>
+            {selectedIcon && (
+              <img src={selectedIcon.icon} alt={selectedIcon.name} className="selected-icon" />
+            )}
+            <div className="dropdown-content">
+                {sabersAndIcons.map((e) => (
+                    <div key={e.id} className="icon-saber-pair" onClick={() => handleIconClick(e)}>
                         <img src={e.icon} alt={e.name + " Icon"} />
                         <img src={e.saber} alt={e.name + " Saber"} />
-                        </div>
-                    ))}
-                </div>
+                    </div>
+                ))}
+            </div>
         </div>
-      );
-  };
+    );
+};

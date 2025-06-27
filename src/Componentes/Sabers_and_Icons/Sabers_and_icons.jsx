991ab6a62b213b68ba1d3aad44521fda8c601fdb
el-nazgul-cansado <francojoaquinbeccari@gmail.com>
2025-06-27 00:00:52 -0300

    arreglado error del dropdown de sabers-and-icons para firefox

--- src/Componentes/Sabers_and_Icons/Sabers_and_icons.jsx
+++ src/Componentes/Sabers_and_Icons/Sabers_and_icons.jsx
@@ -1,33 +1,33 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import styled, { keyframes } from 'styled-components';
-import { useSabersIcons } from "../../context/SabersIconsContext"
+import { useSabersIcons } from "../../context/SabersIconsContext";
 import './Sabers_and_icons.css';
 
 export const Sabers_and_icons = () => {
   const { sabersAndIcons, selectedIcon, handleIconClick } = useSabersIcons();
 
   const [activeCard, setActiveCard] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const handleMouseEnter = (index) => {
     setActiveCard(index);
   };
-  
+
   const handleMouseLeave = () => {
     setActiveCard(null);
   };
 
-  const vibrate = keyframes`
-    0% { box-shadow: 0px 0px 5px ${selectedIcon.color}, 0px 0px 10px ${selectedIcon.color}, 0px 0px 20px ${selectedIcon.color}; } /* Sombra más definida */
-    25% { box-shadow: 0px 0px 8px ${selectedIcon.color}, 0px 0px 15px ${selectedIcon.color}, 0px 0px 25px ${selectedIcon.color}; } /* Sombra más definida */
-    50% { box-shadow: 0px 0px 10px ${selectedIcon.color}, 0px 0px 20px ${selectedIcon.color}, 0px 0px 40px ${selectedIcon.color}; } /* Sombra más definida */
-    75% { box-shadow: 0px 0px 8px ${selectedIcon.color}, 0px 0px 15px ${selectedIcon.color}, 0px 0px 25px ${selectedIcon.color}; } /* Sombra más definida */
-    100% { box-shadow: 0px 0px 5px ${selectedIcon.color}, 0px 0px 10px ${selectedIcon.color}, 0px 0px 20px ${selectedIcon.color}; } /* Sombra más definida */
-  `;
-
   if (!selectedIcon) {
     return <div></div>;
   }
+
+  const vibrate = keyframes`
+    0% { box-shadow: 0px 0px 5px ${selectedIcon.color}, 0px 0px 10px ${selectedIcon.color}, 0px 0px 20px ${selectedIcon.color}; }
+    25% { box-shadow: 0px 0px 8px ${selectedIcon.color}, 0px 0px 15px ${selectedIcon.color}, 0px 0px 25px ${selectedIcon.color}; }
+    50% { box-shadow: 0px 0px 10px ${selectedIcon.color}, 0px 0px 20px ${selectedIcon.color}, 0px 0px 40px ${selectedIcon.color}; }
+    75% { box-shadow: 0px 0px 8px ${selectedIcon.color}, 0px 0px 15px ${selectedIcon.color}, 0px 0px 25px ${selectedIcon.color}; }
+    100% { box-shadow: 0px 0px 5px ${selectedIcon.color}, 0px 0px 10px ${selectedIcon.color}, 0px 0px 20px ${selectedIcon.color}; }
+  `;
 
   const StyledDropdown = styled.div`
     border: 3px solid ${selectedIcon.color};
@@ -35,22 +35,39 @@ export const Sabers_and_icons = () => {
   `;
 
   return (
-    <div className="dropdown">
-      <img src={selectedIcon.icon} alt={selectedIcon.name} className="selected-icon" />
-      <StyledDropdown className={`dropdown-content`} >
-        {sabersAndIcons.map((e, index) => (
-          <div key={e.id} className={`icon-saber-pair hoverIcon${e.color}`} onClick={() => handleIconClick(e)}
-          onMouseEnter={() => handleMouseEnter(index)}
-          onMouseLeave={handleMouseLeave}
-          style={{
-            filter: activeCard !== null && activeCard !== index ? 'grayscale(100%)' : 'none',
-          }}
-          >
-            <img src={e.icon} alt={e.name + " Icon"} />
-            <img src={e.saber} alt={e.name + " Saber"} />
-          </div>
-        ))}
-      </StyledDropdown>
+    <div
+      className="dropdown"
+      onMouseEnter={() => setIsOpen(true)}
+      onMouseLeave={() => setIsOpen(false)}
+    >
+      <img
+        src={selectedIcon.icon}
+        alt={selectedIcon.name}
+        className="selected-icon"
+      />
+
+      {isOpen && (
+        <StyledDropdown className="dropdown-content">
+          {sabersAndIcons.map((e, index) => (
+            <div
+              key={e.id}
+              className={`icon-saber-pair hoverIcon${e.color}`}
+              onClick={() => handleIconClick(e)}
+              onMouseEnter={() => handleMouseEnter(index)}
+              onMouseLeave={handleMouseLeave}
+              style={{
+                filter:
+                  activeCard !== null && activeCard !== index
+                    ? 'grayscale(100%)'
+                    : 'none',
+              }}
+            >
+              <img src={e.icon} alt={e.name + " Icon"} />
+              <img src={e.saber} alt={e.name + " Saber"} />
+            </div>
+          ))}
+        </StyledDropdown>
+      )}
     </div>
   );
 };
